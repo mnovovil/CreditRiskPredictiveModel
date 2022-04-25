b09bf897babb0badbfe3e47a3c0f180df807e573
Lisa Pink <lisapink@Lisas-MBP.lan>
2022-04-24 22:03:50 -0400

    Added file uploader page

--- dashboard/App.py
+++ dashboard/App.py
@@ -2,12 +2,13 @@
 import streamlit as st
 
 from Dashboard import Dashboard
-from Pages import Home, DataAnalysis, LogReg	
+from Pages import Home, DataAnalysis, LogReg, FileUploader
 
 dashboard = Dashboard()
 
 dashboard.Add_Page("Home", Home.page)
 dashboard.Add_Page("Data Analysis", DataAnalysis.page)
 dashboard.Add_Page("Logistic Regression Model", LogReg.page)
+dashboard.Add_Page("File Uploader", FileUploader.page)
 
 dashboard.run()
